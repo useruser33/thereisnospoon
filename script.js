1701cc2776ea11201c4a339ba1332b7bab48d6df
GabinCham <gabin.chameroy@hotmail.com>
2021-08-24 17:14:44 +0200

    commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// No JavaScript was harmed in this process.
+var x = 0
+
+function titilleMoi() {
+
+    x = x + 1;
+    if (x === 0) {
+        document.getElementById('text').innerHTML = 'Titille moi....';
+    }
+    if (x === 1) {
+        document.getElementById('text').innerHTML = 'Encore stp';
+    }
+    if (x === 3) {
+        document.getElementById('text').innerHTML = 'Encoore';
+    }
+    if (x === 4) {
+        document.getElementById('text').innerHTML = 'Encoooore';
+    }
+    if (x === 5) {
+        document.getElementById('text').innerHTML = 'Encoooooore';
+    }
+    if (x === 6) {
+        document.getElementById('text').innerHTML = 'Encooooooore';
+    }
+    if (x === 7) {
+        document.getElementById('text').innerHTML = 'Ouuuuui';
+    }
+    if (x === 8) {
+        document.getElementById('text').innerHTML = 'Grrreeeeee...';
+    }
+
+    if (x === 9) {
+        window.location.href = 'https://google.com';
+    }
+}
